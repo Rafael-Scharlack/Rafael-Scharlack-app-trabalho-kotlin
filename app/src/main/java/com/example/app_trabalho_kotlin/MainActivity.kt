b68de2971b2cb764abc9e7d23b4b8f0d32ccb15f
Rafael-Scharlack <rafaelcatapani@gmail.com>
2026-03-15 23:32:02 -0300

    Implementa NavController na MainActivity para gerenciar navegação entre telas com Navigation Compose

--- app/src/main/java/com/example/app_trabalho_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/app_trabalho_kotlin/MainActivity.kt
@@ -8,7 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.app_trabalho_kotlin.screens.LoginScreen
+import com.example.app_trabalho_kotlin.screens.MenuScreen
+import com.example.app_trabalho_kotlin.screens.PedidosScreen
+import com.example.app_trabalho_kotlin.screens.PerfilScreen
 import com.example.app_trabalho_kotlin.ui.theme.ApptrabalhokotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,8 +23,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ApptrabalhokotlinTheme {
+                val navController = rememberNavController()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(modifier = Modifier.padding(innerPadding))
+                    NavHost(navController = navController, startDestination = "login") {
+                        composable("login") { LoginScreen(modifier = Modifier.padding(innerPadding)) }
+                        composable("menu") { MenuScreen(modifier = Modifier.padding(innerPadding)) }
+                        composable("perfil") { PerfilScreen(modifier = Modifier.padding(innerPadding)) }
+                        composable("pedidos") { PedidosScreen(modifier = Modifier.padding(innerPadding)) }
+                    }
                 }
             }
         }
